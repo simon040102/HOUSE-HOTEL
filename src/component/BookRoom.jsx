37354f113e71adbe19f04433aae64b85b3a6c64e
simon040102 <simon040102@gmail.com>
2022-09-04 11:45:05 +0800

    debug

--- src/component/BookRoom.jsx
+++ src/component/BookRoom.jsx
@@ -66,11 +66,10 @@ const BookRoom = (props) => {
         end: new Date(formatDate(endDate)),
       });
       setIsWeekend(result.length);
-      console.log(alreadyBooked);
+      await checkSameDate(startDate, endDate);
        if (alreadyBooked == true) {
          Swal.fire('所選日期已被預訂');
-         setStartDate(new Date());
-         setEndDate(new Date());
+        setEndDate(startDate)
          return;
        }
     } catch (error) {}
@@ -80,7 +79,7 @@ const BookRoom = (props) => {
      chosenCheckoutDay();
      dateChange();
      checkSameDate(startDate, endDate);
-   }, [startDate, endDate, alreadyBooked]);
+   }, [startDate, endDate,alreadyBooked, roomBooked]);
 const chosenCheckInDay = () => {
   setCheckIn([]);
   try {
@@ -136,7 +135,7 @@ const chosenCheckoutDay = () => {
       let check2 = (new Date(endDay) >= newStartDay) && (new Date(endDay) <= date1);
       let check3 = date0 >= new Date(newStartDay) && date0 <= new Date(endDay);
       let check4 =date1 >= new Date(startDay) && (newStartDay <= new Date(endDay));
-      console.log(check1 , check2 , check3 , check4)
+ 
       if (check == true) {
         return;
       } else if (check1 || check2 || check3 || check4) {
@@ -158,12 +157,7 @@ const chosenCheckoutDay = () => {
       Swal.fire('請輸入電話號碼');
       return;
     }
-    if (alreadyBooked == true) {
-         Swal.fire('所選日期已被預訂');
-         setStartDate(new Date());
-         setEndDate(new Date());
-         return;
-       }
+    
     setIsLoading(true);
     axios
       .post(`https://pure-harbor-20136.herokuapp.com/reserve/${Id}`, customer)
@@ -221,7 +215,7 @@ const chosenCheckoutDay = () => {
                 selected={startDate}
                 onChange={(date) => {
                   setStartDate(date);
-                  setEndDate(date);
+
                   dateChange();
                   // chosenEndDay();
                 }}
@@ -271,7 +265,7 @@ const chosenCheckoutDay = () => {
                 <button
                   className=" w-full"
                   type="button"
-                  disabled={totalDays < 0}
+                  disabled={totalDays < 0 || alreadyBooked == true || totalDays==0}
                   onClick={reserve}
                 >
                   確認送出
